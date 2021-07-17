8555e8eca574a9e56d98c089f3b69477b70bbf93
Ari Marin <ari.marin@oscar.fi>
2021-07-17 16:20:12 +0300

    Added file and reading content

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args)
+
+    // Args to variables
+    // let binary = &args[0]; // Not needed
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Error reading the file");
+
+    println!("With text:\n{}", contents);
 }
